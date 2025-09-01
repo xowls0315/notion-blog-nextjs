3b6e37d2db25b25f116d27c29e1266409d74b003
t.jin_01 <601135@naver.com>
2025-09-01 18:06:35 +0900

    57강까지 완료

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -1,23 +1,34 @@
 import Link from 'next/link';
+import { Button } from '@/components/ui/button';
 
 export default function Header() {
   return (
     <header className="bg-background sticky top-0 z-50 border-b">
       <div className="container flex h-[var(--header-height)] items-center px-4">
-        <Link href="/" className="text-xl font-semibold">
-          <span className="font-bold">t.jin_01 블로그</span>
-        </Link>
-        <nav className="ml-auto flex items-center gap-4">
-          <Link href="/" className="hover:text-primary font-medium">
-            홈
-          </Link>
-          <Link href="/blog" className="hover:text-primary font-medium">
-            블로그
-          </Link>
-          <Link href="/about" className="hover:text-primary font-medium">
-            소개
-          </Link>
-        </nav>
+        <div className="grid w-full grid-cols-3 items-center">
+          <div className="flex items-center justify-start">
+            <Link href="/" className="text-xl font-semibold">
+              <span className="font-bold">t.jin_01 블로그</span>
+            </Link>
+          </div>
+          <nav className="flex items-center justify-center gap-4">
+            <Link href="/" className="hover:text-primary font-medium">
+              홈
+            </Link>
+            <Link href="/blog" className="hover:text-primary font-medium">
+              블로그
+            </Link>
+            <Link href="/about" className="hover:text-primary font-medium">
+              소개
+            </Link>
+          </nav>
+
+          <div className="flex items-center justify-end">
+            <Button asChild size="sm" className="gap-2">
+              <Link href="/blog/write">글쓰기</Link>
+            </Button>
+          </div>
+        </div>
       </div>
     </header>
   );
