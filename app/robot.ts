454f75199b21e4807bba9d08c79212f3329e024c
t.jin_01 <601135@naver.com>
2025-09-02 16:58:42 +0900

    69강 수정

--- app/robot.ts
+++ app/robot.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/api/', '/admin/'],
     },
-    sitemap: 'https://notion-blog-nextjs.vercel.app/sitemap.xml',
-    // sitemap: `${process.env.NEXT_PUBLIC_SITE_URL}/sitemap.xml`,
+    // sitemap: 'https://notion-blog-nextjs.vercel.app/sitemap.xml',
+    sitemap: `${process.env.NEXT_PUBLIC_SITE_URL}/sitemap.xml`,
   };
 }
